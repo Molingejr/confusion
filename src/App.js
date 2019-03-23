991001c5b7c86c916a8d0aedb997ea55deb9f88e
Molingejr <molingejr@gmail.com>
2019-03-23 12:45:59 +0100

    Combined Reducers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
