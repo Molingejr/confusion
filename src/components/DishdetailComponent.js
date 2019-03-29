4081fd8d50383de3253f9fd5dff33a4ddfca6b6d
Molingejr <molingejr@gmail.com>
2019-03-30 00:32:42 +0100

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -110,11 +111,12 @@ function RenderComments({ comments, postComment, dishId }) {
 
     const comments_display = comments.map((comment) => {
         return (
-            <li key={comment.id}>{comment.comment} <br /> <br />
-                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-                <br /><br />
-            </li>
-
+            <Fade in>
+                <li key={comment.id}>{comment.comment} <br /> <br />
+                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                    <br /><br />
+                </li>
+            </Fade>
         );
     });
 
@@ -126,8 +128,11 @@ function RenderComments({ comments, postComment, dishId }) {
                         <CardTitle>Comments</CardTitle>
                         <CardText>
                             <ul className="list-unstyled">
-                                {comments_display}
+                                <Stagger in>
+                                    {comments_display}
+                                </Stagger>
                             </ul>
+
                             <CommentForm dishId={dishId} postComment={postComment} />
                         </CardText>
 
@@ -146,13 +151,18 @@ function RenderDish({ dish }) {
         return (
             <div className="col-12 col-md-5 m-1">
                 <p>{dish.description}</p>
-                <Card>
-                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card>
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             </div>
 
         );
